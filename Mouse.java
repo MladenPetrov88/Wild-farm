3f36c6260d322e4d5d6aac40d6bf9f787cf7eb59
Mladen Petrov <mladen.petrov88@gmail.com>
2020-03-22 12:21:35 +0200

    Add files via upload

---
+++ Mouse.java
@@ -0,0 +1,24 @@
+package WildFarm;
+
+public class Mouse extends Mammal {
+
+    private static final String INVALID_FOOD = "Mice are not eating that type of food!";
+
+    public Mouse(String name, String type, Double weight, String livingRegion) {
+        super(name, type, weight, livingRegion);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("SQUEEEAAAK!");
+    }
+
+    @Override
+    public void eat(Food food) {
+        if (!(food instanceof Vegatable)){
+            super.setFoodEaten(new Vegatable(0));
+            throw new IllegalArgumentException(INVALID_FOOD);
+        }
+        super.setFoodEaten(food);
+    }
+}
---
+++ Vegatable.java
@@ -0,0 +1,7 @@
+package WildFarm;
+
+public class Vegatable extends Food {
+    public Vegatable(Integer quantity) {
+        super(quantity);
+    }
+}
